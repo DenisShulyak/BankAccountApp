0bd68c8bf4c33804eb3b16d27fc6f2ad4837f21a
Denis <denshufly@mail.ru>
2019-06-10 23:58:51 +0600

    BankAccount

---
+++ BankAccountApp/BankAccount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BankAccountApp
+{
+    [Synchronization]
+    public class BankAccount  : ContextBoundObject
+    {
+
+        private int account = 500;
+        private object lockObject = new object();
+        public void WithdrawalAndAccrual()//Снятие и пополнение счета рандомным образом 
+        {
+           
+                Random random = new Random();
+                int randomNumber = random.Next(-1000, 1000);
+               
+                // DateTime.TimeOfDay time = DateTime.Now.TimeOfDay;
+
+                if (randomNumber > 0)
+                {
+                    var currentThread = Thread.CurrentThread;
+                    Console.WriteLine($"{currentThread.Name} начал свою работу");
+                  
+                    account += randomNumber;
+                    Console.WriteLine($"На счет начисленно {randomNumber}. Текущий счет {account}. Начислел {currentThread.Name} поток");
+                    Console.WriteLine($"{currentThread.Name} закончил свою работу");
+                }
+                else if (randomNumber < 0)
+                {
+                    var currentThread = Thread.CurrentThread;
+                    Console.WriteLine($"{currentThread.Name} начал свою работу");
+                  
+                    account += randomNumber;
+                    Console.WriteLine($"Со счета списано {-randomNumber}. Текущий счет {account}. Снял {currentThread.Name} поток");
+                    Console.WriteLine($"{currentThread.Name} закончил свою работу");
+
+                }
+            
+        }
+
+    }
+}
---
+++ BankAccountApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace BankAccountApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*var threads = new Thread[20];
+            BankAccount bankAccount = new BankAccount();
+            for (int i = 0; i < threads.Length; i++)
+            {
+                var thread = new Thread(bankAccount.WithdrawalAndAccrual);
+                thread.Name = $"Поток номер {thread.ManagedThreadId}";
+                threads[i] = thread;
+            }
+
+          
+            for(int i = 0; i < threads.Length;i++)
+            {
+                threads[i].Start();
+            }
+            */
+        BankAccount bankAccount = new BankAccount();
+            var timeMinute = DateTime.Now.Minute + 1;
+            var timeSecond = DateTime.Now.Second;
+           
+
+            while (DateTime.Now.TimeOfDay.Minutes != timeMinute || DateTime.Now.Second != timeSecond)
+            {
+            var thread = new Thread(bankAccount.WithdrawalAndAccrual);
+            thread.Name = $"Поток номер {thread.ManagedThreadId}";
+            thread.Start();
+            }
+
+            Console.WriteLine(1);
+            Console.ReadLine();
+            
+        }
+            
+  
+
+
+        /*
+         var threads = new Thread[20];
+            var printer = new Printer();
+            for(int i = 0;i < threads.Length;i++)
+            {
+                var thread = new Thread(printer.Print);
+                thread.Name = $"Поток номер {thread.ManagedThreadId}";
+                threads[i] = thread;
+            }
+
+            foreach(var thread in threads)
+            {
+                thread.Start();
+            }
+
+            Console.ReadLine();
+            */
+    }
+}
